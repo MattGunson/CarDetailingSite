79960fd1455ec9f9139022d3923f37305e42bf88
Matt Gunson <mgunson@qualtrics.com>
2023-10-27 14:52:45 -0600

    Add partial profile content

--- site-frontend/app/profile/[service]/page.tsx
+++ site-frontend/app/profile/[service]/page.tsx
@@ -1,4 +1,14 @@
-export default function ServiceDetails() {
+import { options } from "../../api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+
+export default async function ServiceDetails() {
+  const session = await getServerSession(options)
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div>
--- site-frontend/app/profile/page.tsx
+++ site-frontend/app/profile/page.tsx
@@ -1,23 +1,54 @@
+import { options } from "../api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 interface card {
   description: string;
+  content: kvPair[];
 }
 
-const cards: card[] = [
-  { description: "Hello User!" },
-  { description: "My cars window" },
-  { description: "My services" },
-  { description: "My information" }
+interface kvPair {
+  key: string;
+  val: string;
+}
+
+let cards: card[] = [
+  { description: "My Cars", content: [] },
 ];
 
-export default function Blog() {
+export default async function Profile() {
+  const session = await getServerSession(options)
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
+  }
+
+  let user = session.user
+  let cars = null // TODO: add query for cars
+  let services = null // TODO: add query for services
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-wrap">
+        <div className="m-4 bg-slate-200 h-[25vh] w-[50vh]">
+          {user ? <>
+            {"My Profile"}
+            <div className="ml-4">{"name: " + user.name}</div>
+            <div className="ml-4">{"email: " + user.email}</div>
+            <div className="ml-4">{"image: " + user.image}</div>
+          </> : <></>}
+        </div>
+
         {cards.map((card) => (
           <div key={card.description} className="m-4 bg-slate-200 h-[25vh] w-[50vh]">
             {card.description}
+            {card.content.map((content) => (
+              <div key={content.key} className="ml-4">
+                {content.key + ": " + content.val}
+              </div>
+            ))
+            }
           </div>
         ))}
         <div className="m-4 bg-slate-300 h-[25vh] w-[50vh]">
